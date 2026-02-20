b021200132537500cbfcf365de46756bfa9bbd27
Cristianh221 <crizh221@gmail.com>
2026-02-19 21:17:54 -0500

    feat: implementación de api rest y base de datos en memoria

---
+++ Task_Api/Task_Api/Models/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio")]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public bool IsCompleted { get; set; } = false;
+    }
+}
